b9d5a662bfdcc6567fe642ccf371fab1cc6e1c50
Crisitan <crisluis2512@gmail.com>
2026-08-06 22:07:49 -0300

    feat: shadcn calendar added

--- src/app/components/DiffDate.tsx
+++ src/app/components/DiffDate.tsx
@@ -1,9 +1,30 @@
+"use client"
+
+import { Calendar } from "@/components/ui/calendar"
+import React, { useState } from "react"
+
 export const DiffDate = () => {
-    return(
-        <section className="h-full bg-amber-500 flex flex-3 gap-90 items-center justify-center">
-            <div>data 1</div>
+    const [date, setDate] = useState<Date | undefined>(new Date())
+
+    return (
+        <section className="h- bg-amber-500 flex gap-20 items-center justify-center">
+            <div>
+                <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    className="rounded-lg border"
+                />
+            </div>
             <div>resultado-data</div>
-            <div>data2</div>
+            <div>
+                <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    className="rounded-lg border"
+                />
+            </div>
         </section>
     )
 }
